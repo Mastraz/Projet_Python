f99bfb0dc0547c95f8bd6ed84de2be341a71f4e9
Mastraz <nolan.nedelec@gmail.com>
2026-02-04 08:05:32 +0100

    small fix

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def analyze_movie(title: str, synopsis: str):
         "recommendations": recommendations
     }
 
-# --- LANCEMENT ---
+# --- LANCEMENT SECURITAIRE ---
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
